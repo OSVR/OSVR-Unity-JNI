1ee56015aebc8f3601ce56e151a0681893981cec
Greg Aring <greg@sensics.com>
2017-09-21 10:48:48 -0400

    Updated code for camera support -- not yet working

--- app/src/main/java/org/osvr/osvrunityjni/OsvrJNIWrapper.java
+++ app/src/main/java/org/osvr/osvrunityjni/OsvrJNIWrapper.java
@@ -36,6 +36,13 @@ import static android.opengl.EGL14.eglMakeCurrent;
 import android.util.DisplayMetrics;
 import java.lang.reflect.Field;
 
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.KeyEvent;
+import java.io.IOException;
+
+
 public class OsvrJNIWrapper {
     private static long EGL_NO_DISPLAY_ERROR = 10;
     private static long EGL_INIT_ERROR = 11;
@@ -52,6 +59,143 @@ public class OsvrJNIWrapper {
     private Field unityCurrentActivity;
     private boolean librariesLoaded = false;
     private static boolean loggingEnabled = false;
+
+    private static boolean sPaused = false;
+    private static boolean sCameraEnabled = false;
+    private static boolean sIsMissingNativeButtonFuncs = false; // native button jni funcs may not be loaded
+
+    static SurfaceTexture sCameraTexture;
+    static int sCameraPreviewWidth = -1;
+    static int sCameraPreviewHeight = -1;
+    static Camera sCamera;
+
+    public static native void reportFrame(byte[] data, long width, long height);
+
+    public static native void reportKeyDown(int keyCode); // might change the signature
+    public static native void reportKeyUp(int keyUp); // might change the signature
+
+    static Camera.PreviewCallback sPreviewCallback = new Camera.PreviewCallback() {
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            //Log.d(TAG, "Got onPreviewFrame");
+            if(!sPaused) {
+                OsvrJNIWrapper.reportFrame(
+                        data, sCameraPreviewWidth, sCameraPreviewHeight);
+            }
+        }
+    };
+
+    private static void setCameraParams() {
+        Camera.Parameters parms = sCamera.getParameters();
+        parms.setRecordingHint(true);
+        parms.setVideoStabilization(false);
+        parms.setPreviewSize(640, 480);
+        Camera.Size size = parms.getPreviewSize();
+        sCameraPreviewWidth = size.width;
+        sCameraPreviewHeight = size.height;
+        sCamera.setParameters(parms);
+
+        int[] fpsRange = new int[2];
+        Camera.Size mCameraPreviewSize = parms.getPreviewSize();
+        parms.getPreviewFpsRange(fpsRange);
+        String previewFacts = mCameraPreviewSize.width + "x" + mCameraPreviewSize.height;
+        if (fpsRange[0] == fpsRange[1]) {
+            previewFacts += " @" + (fpsRange[0] / 1000.0) + "fps";
+        } else {
+            previewFacts += " @[" + (fpsRange[0] / 1000.0) +
+                    " - " + (fpsRange[1] / 1000.0) + "] fps";
+        }
+       // Log.i(TAG, "Camera config: " + previewFacts);
+
+        sCameraPreviewWidth = mCameraPreviewSize.width;
+        sCameraPreviewHeight = mCameraPreviewSize.height;
+    }
+
+    private static void openCamera() {
+        if(sCameraEnabled && sCamera == null) {
+            sCameraTexture = new SurfaceTexture(123);
+            sCamera = Camera.open();
+            setCameraParams();
+            try {
+                sCamera.setPreviewTexture(sCameraTexture);
+            } catch (IOException ex) {
+                //Log.d(TAG, "Error on setPreviewTexture: " + ex.getMessage());
+                throw new RuntimeException("error during setPreviewTexture");
+            }
+            //mCamera.setPreviewCallbackWithBuffer(this);
+            sCamera.setPreviewCallback(sPreviewCallback);
+            sCamera.startPreview();
+        }
+    }
+
+    protected static void stopCamera() {
+        if(sCamera != null) {
+            sCamera.setPreviewCallback(null);
+            sCamera.stopPreview();
+            sCamera.release();
+            sCamera = null;
+        }
+    }
+
+    public static void enableCamera() {
+        sCameraEnabled = true;
+    }
+
+    public static void disableCamera() {
+        sCameraEnabled = false;
+        stopCamera();
+    }
+
+    public static void onResume() {
+        sPaused = false;
+        openCamera();
+    }
+
+    public static void onStop() {
+        sPaused = true;
+        stopCamera();
+    }
+
+    public static void onPause() {
+        sPaused = true;
+        stopCamera();
+    }
+
+    public static boolean onKeyDown(int keyCode, KeyEvent keyEvent) {
+        // The native code to push key down events might not be loaded,
+        // we'll try to call it and gracefully recover if we fail.
+        // After a failure, we'll stop trying
+        if(sIsMissingNativeButtonFuncs) {
+            return false;
+        }
+        try {
+            reportKeyDown(keyCode);
+            return true;
+        } catch(Exception e) { // @TODO: narrow down this exception?
+            sIsMissingNativeButtonFuncs = true;
+            return false;
+        }
+    }
+
+    public static boolean onKeyUp(int keyCode, KeyEvent keyEvent) {
+        // The native code to push key down events might not be loaded,
+        // we'll try to call it and gracefully recover if we fail.
+        // After a failure, we'll stop trying
+        if(sIsMissingNativeButtonFuncs) {
+            return false;
+        }
+        try {
+            reportKeyUp(keyCode);
+            return true;
+        } catch(Exception e) { // @TODO: narrow down this exception?
+            sIsMissingNativeButtonFuncs = true;
+            return false;
+        }
+    }
+
+    public static void onSurfaceChanged() {
+        openCamera();
+    }
 
     //load OSVR libraries
     public void loadLibraries() {
@@ -74,6 +218,7 @@ public class OsvrJNIWrapper {
             System.loadLibrary("osvrJointClientKit");
             System.loadLibrary("com_osvr_android_jniImaging");
             System.loadLibrary("com_osvr_android_sensorTracker");
+            System.loadLibrary("org_osvr_android_moverio");
             System.loadLibrary("com_osvr_Multiserver");
             System.loadLibrary("org_osvr_filter_deadreckoningrotation");
             System.loadLibrary("org_osvr_filter_oneeuro");
@@ -158,14 +303,14 @@ public class OsvrJNIWrapper {
     }
 
     //create a gl context for the opengl toolkit
-    public static long createContext(){
+    public static long createContext() {
         mEglDisplay = eglGetCurrentDisplay();
         mEglSurface = eglGetCurrentSurface(EGL14.EGL_DRAW);
         mEglConfig = null;
         if (mEglDisplay == null) {
             mEglDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
             if (mEglDisplay == EGL14.EGL_NO_DISPLAY) {
-               // logMsg("eglGetDisplay");
+                // logMsg("eglGetDisplay");
                 return EGL_NO_DISPLAY_ERROR;
             }
 
@@ -178,7 +323,7 @@ public class OsvrJNIWrapper {
         }
 
         if (mEglConfig == null) {
-            int[] eglConfigAttribList = new int[] {
+            int[] eglConfigAttribList = new int[]{
                     EGL14.EGL_RED_SIZE, 8,
                     EGL14.EGL_GREEN_SIZE, 8,
                     EGL14.EGL_BLUE_SIZE, 8,
@@ -196,7 +341,7 @@ public class OsvrJNIWrapper {
         }
 
         if (mEglContext == null) {
-            int[] eglContextAttribList = new int[] {
+            int[] eglContextAttribList = new int[]{
                     EGL14.EGL_NONE
             };
             mEglContext = EGL14.eglCreateContext(mEglDisplay, mEglConfig,
@@ -208,7 +353,4 @@ public class OsvrJNIWrapper {
         }
         return mEglContext.getNativeHandle();
     }
-
-
-
 }
